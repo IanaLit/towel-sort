6a905f909d48b109c739e6ef2c2b7ce59d8a707b
iana <litvinova.iana1212@gmail.com>
2021-03-01 21:25:20 +0700

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+      if (matrix === undefined || matrix.length < 1  ) {
+      return [];
+      }
+      else {
+    
+    let array = [...matrix];
+    array.map((curr, i) => {
+      if( i%2 !== 0){
+         curr.reverse();
+      }});
+      return array.reduce((acc, val) => acc.concat(val), []);
+    }}
